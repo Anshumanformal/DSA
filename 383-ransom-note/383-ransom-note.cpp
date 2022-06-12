6c73d0a8b335414d48020b637bc1c54f223dba2e
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-06-12 17:01:34 +0530

    Time: 29 ms (36.10%), Space: 8.7 MB (72.81%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> map;
+        
+        for(int i=0; i<magazine.size(); ++i)
+            ++map[magazine[i]];
+        
+        for(int i=0; i<ransomNote.size(); ++i)
+            if(--map[ransomNote[i]] < 0)
+                return false;
+        
+        return true;
+    }
+};
