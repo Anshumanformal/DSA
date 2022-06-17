01ce686bb194193da138b99df3a4304a6ec9efda
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-06-17 21:00:14 +0530

    Time: 14 ms (44.42%), Space: 10.1 MB (61.57%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -2,11 +2,29 @@ class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
         
-        sort(nums.begin(), nums.end(), greater<int>());
-        return nums[k-1];
+        // 1st solution
+        // sort(nums.begin(), nums.end(), greater<int>());
+        // return nums[k-1];
+        
+        // 2nd solution - C++ STL has nth_element built-in function
+        nth_element(nums.begin(), nums.begin() + k - 1, nums.end(), greater<int>());
+        return nums[k - 1];
+        
+        // 3rd solution - C++ STL has partial_sort built-in function
+        // partial_sort(nums.begin(), nums.begin() + k, nums.end(), greater<int>());
+        // return nums[k - 1];
         
         // since k'th largest element is asked, hence we create a minHeap for this question.
-        // priority_queue<int,vector<int>,greater<int>> minHeap;
+        // priority_queue<int,vector<int>,greater<int>> pq;
+        // for(int num : nums){
+        //     pq.push(num);
+        //     if(pq.size() > k)
+        //         pq.pop();
+        // }
+        // return pq.top();
+        
+        // A maxHeap can also be created.
+        
         
     }
 };
