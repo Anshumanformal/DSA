625f54b557bc6192a52b882441013179fffdee47
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-06-17 16:14:21 +0530

    Time: 318 ms (51.06%), Space: 114.1 MB (85.21%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -8,6 +8,36 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+// 152 ms solution(found in submissions)------------------------------------
+
+// bool isPalindrome(ListNode* head) {
+//         ListNode* h1=nullptr, *p1=head, *p2=head;
+//         if(head == nullptr || head->next == nullptr)
+//             return true;
+        
+//         while(p2 && p2->next){
+//             ListNode* tmp = p1;
+//             p1 = p1->next;
+//             p2 = p2->next->next;
+            
+//             //reverse the first half of list
+//             tmp->next = h1;
+//             h1 = tmp;
+//         }
+//         if(p2 != nullptr) // odd numbers
+//             p1=p1->next;
+//         while(p1 && h1){
+//             if(p1->val != h1->val)
+//                 return false;
+//             p1 = p1->next;
+//             h1 = h1->next;
+//         }
+//         return !(p1 || h1);
+        
+//     }
+// };
+
 class Solution {
 public:
     
