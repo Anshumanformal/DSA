b7d45e1f7f612496daacd5ff6a3cb34a14adb543
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-06-20 19:21:27 +0530

    Time: 41 ms (6.04%), Space: 12.5 MB (73.91%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,35 +1,37 @@
-typedef pair<int,string> P;
-
 class Solution {
 private:
-     struct MyComp {
-        bool operator() (const pair<int, string>& a, const pair<int, string>& b) {
-            if(a.first != b.first) {
-                return a.first > b.first;
-            }
-            else {
-                return a.second < b.second;
-            }
-        }
+    struct comparator{
+      bool operator()(const pair<int,string>& p1, const pair<int,string>& p2){
+          if(p1.first != p2.first)
+              // here, we return the value of that element which is lexicographically
+              // greater than the other
+              return p1.first > p2.first;
+          else return p1.second < p2.second;
+      }  
     };
 
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
         
-        unordered_map<string, int> hashmap;
-        for(string& word : words) {
-            hashmap[word] += 1;
+        unordered_map<string,int> hashMap;
+        // put all words from vector to hashMap
+        for(string& word : words)
+            hashMap[word]+=1;
+        
+        priority_queue<pair<int,string>,vector<pair<int,string>>, comparator> pq;
+        
+        for(auto it = hashMap.begin(); it != hashMap.end(); ++it){
+            pq.push({it->second, it->first});
+            if(pq.size() > k)
+                pq.pop();
         }
-        priority_queue<pair<int, string>, vector<pair<int, string>>, MyComp> pq;
-        for(auto it = hashmap.begin(); it != hashmap.end(); ++it) {
-            pq.push(make_pair(it->second, it->first));
-            if(pq.size() > k) pq.pop();
-        }
+        
         vector<string> res;
-        while(!pq.empty()) {
+        while(pq.size()){
             res.insert(res.begin(), pq.top().second);
             pq.pop();
         }
+        
         return res;
     }
 };
