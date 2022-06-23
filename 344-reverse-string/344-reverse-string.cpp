84240b57a8ff7eff5ccd6ab403c9c78f65e46b65
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-06-23 18:12:27 +0530

    Time: 35 ms (42.70%), Space: 23.3 MB (40.61%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -6,9 +6,15 @@ public:
         // reverse(s.begin(), s.end());
         
         // 2nd solution - using two pointer approach
-        int i = 0, j = s.size() - 1;
-        while(i < j){
-            swap(s[i++], s[j--]); 
+        // int i = 0, j = s.size() - 1;
+        // while(i < j){
+        //     swap(s[i++], s[j--]); 
+        // }
+        
+        // 3rd solution - using half iteration
+        int n = s.size();
+        for(int i = 0; i < n/2; i++) {
+            swap(s[i], s[n - 1 - i]); // here we subtract the i'th distance which is already travelled by the loop from n-1
         }
         
     }
