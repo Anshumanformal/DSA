cf86a0d3f1f97dc796f2faa30141b871a1f52fb2
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-06-27 22:53:01 +0530

    Time: 66 ms (5.15%), Space: 15 MB (46.29%) - LeetHub

--- 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -18,16 +18,23 @@ public:
         if(head==nullptr) return head;
         
         // If the node to be removed is at head, then simply increment the head by 1.
-        while(head!=nullptr and head->val == val)
+        ListNode* toBeDeleted;
+        while(head!=nullptr and head->val == val){
+            toBeDeleted = head;
             head = head->next;
+            delete toBeDeleted; // to prevent memory leak
+        }
+            
         
         // Create two temporary pointers.
         ListNode* curr = head;
         ListNode* prev = nullptr;
         while(curr!=nullptr){
             if(curr->val == val){
+                toBeDeleted = curr;
                 prev->next = curr->next;
                 curr = curr->next;
+                delete toBeDeleted;
             }
             else {
                 prev = curr;
