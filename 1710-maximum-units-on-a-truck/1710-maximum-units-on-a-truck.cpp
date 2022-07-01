5444c58dc8ba224721ef93301bfbd779bb27794f
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-07-01 12:10:50 +0530

    Time: 58 ms (68.66%), Space: 16.7 MB (47.05%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,29 +1,59 @@
 class Solution {
 public:
     
-    // descending order sorting on the basis of number of units
-    static bool comp(vector<int> a, vector<int> b){
-        if(a[1] > b[1])
-            return true;
-        return false;
-    }
+    // Method 1 - using custom sorting 
     
-    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+    // descending order sorting on the basis of number of units
+//     static bool comp(vector<int> a, vector<int> b){
+//         if(a[1] > b[1])
+//             return true;
+//         return false;
+//     }
+    
+//     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
         
-        sort(boxTypes.begin(), boxTypes.end(), comp);
-        int units=0;
+//         sort(boxTypes.begin(), boxTypes.end(), comp);
+//         int units=0;
         
-        for(int i=0; i<boxTypes.size(); i++){
-            if(boxTypes[i][0] <= truckSize){
-                units += boxTypes[i][0] * boxTypes[i][1]; //take all the boxes and add the units
-                truckSize -= boxTypes[i][0];
-            }
-            else if(boxTypes[i][0] > truckSize){ // truckSize is less than the number of boxes
-                units += truckSize * boxTypes[i][1];
-                break;
-            }
-        }
+//         for(int i=0; i<boxTypes.size(); i++){
+//             if(boxTypes[i][0] <= truckSize){
+//                 units += boxTypes[i][0] * boxTypes[i][1]; //take all the boxes and add the units
+//                 truckSize -= boxTypes[i][0];
+//             }
+//             else if(boxTypes[i][0] > truckSize){ // truckSize is less than the number of boxes
+//                 units += truckSize * boxTypes[i][1];
+//                 break;
+//             }
+//         }
         
-        return units;
-    }
+//         return units;
+//     }
+    
+     // Method 2 - using Priority queue(maxHeap)
+     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+     
+         priority_queue<pair<int,int>> pq;
+         for(int i=0; i<boxTypes.size(); i++)
+             pq.push({boxTypes[i][1], boxTypes[i][0]});
+         
+         int ans=0;
+         while(pq.size()){
+             if(pq.top().second < truckSize){
+                 ans += pq.top().second * pq.top().first;
+                 truckSize -= pq.top().second;
+                 pq.pop();
+             }
+             else if(pq.top().second >= truckSize){
+                 // when the item quantity exceeds truckSize, we multiply the difference with the
+                 // quantity and break the loop
+                 ans += truckSize * pq.top().first;
+                 pq.pop();
+                 break;
+             }
+             
+         }
+         
+         return ans;         
+         
+     }
 };
