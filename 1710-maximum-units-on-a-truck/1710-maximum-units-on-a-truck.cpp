9f1736c04ca6e78fe780d6dd63187c7f5b14fb96
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-07-01 11:19:37 +0530

    Time: 320 ms (13.54%), Space: 49.5 MB (14.24%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     
     // descending order sorting on the basis of number of units
-    static bool comp(vector<int> a, vector<int> b)
-    {
-        if(a[1]>b[1])
+    static bool comp(vector<int> a, vector<int> b){
+        if(a[1] > b[1])
             return true;
         return false;
     }
+    
     int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
-        sort(boxTypes.begin(), boxTypes.end(), comp);     //Sorting given array in descending order of numberOfUnitsPerBoxi
-        int units = 0;
-        for(int i=0; i<boxTypes.size(); i++)
-        {
-            if(boxTypes[i][0]<=truckSize)                 //if no. of boxes is less than or equal to trucksize
-            {
-                units += boxTypes[i][0]*boxTypes[i][1];   //take all the boxes and addd the units
-                truckSize-=boxTypes[i][0];                //decrement the truckSize by numberOfBoxesi
+        
+        sort(boxTypes.begin(), boxTypes.end(), comp);
+        int units=0;
+        
+        for(int i=0; i<boxTypes.size(); i++){
+            if(boxTypes[i][0] <= truckSize){
+                units += boxTypes[i][0] * boxTypes[i][1]; //take all the boxes and add the units
+                truckSize -= boxTypes[i][0];
             }
-            else if(boxTypes[i][0]>truckSize)             //if truckSize is less than numberOfBoxesi
-            {
-                units += truckSize*boxTypes[i][1];        //take the boxes equal to remaining size and add the units
+            else if(boxTypes[i][0] > truckSize){ // truckSize is less than the number of boxes
+                units += truckSize * boxTypes[i][1];
                 break;
             }
         }
+        
         return units;
     }
 };
