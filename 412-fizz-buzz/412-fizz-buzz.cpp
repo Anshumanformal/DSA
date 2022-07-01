79ec7ba1a3cef13228fb2d927f6a82987a621846
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-07-01 19:58:53 +0530

    Time: 3 ms (85.66%), Space: 7.9 MB (64.27%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -3,17 +3,40 @@ public:
     vector<string> fizzBuzz(int n) {
         vector<string> answer;
         
-        for(int i=1; i<=n; i++){
-            if(i % 3 == 0 and i % 5 == 0)
+        // Method - 1 (simple method) - does unnecessary extra evaluation of the same values like
+        // i % 3 and i % 5
+        
+//         for(int i=1; i<=n; ++i){
+//             if(i % 3 == 0 and i % 5 == 0)
+//                 answer.push_back("FizzBuzz");
+//             else if(i % 3 == 0)
+//                 answer.push_back("Fizz");
+//             else if(i % 5 == 0)
+//                 answer.push_back("Buzz");
+//             else
+//                 answer.push_back(to_string(i));
+//         }
+        
+//         return answer;
+        
+        // Method - 2(storing the result of i % 3 and i % 5 into some variable)
+        
+        for(int i=1; i<=n; ++i){
+            
+            bool fizz = i % 3 == 0;
+            bool buzz = i % 5 == 0;
+            
+            if(fizz and buzz)
                 answer.push_back("FizzBuzz");
-            else if(i % 3 == 0)
+            else if(fizz)
                 answer.push_back("Fizz");
-            else if(i % 5 == 0)
+            else if(buzz)
                 answer.push_back("Buzz");
             else
                 answer.push_back(to_string(i));
         }
         
         return answer;
+        
     }
 };
