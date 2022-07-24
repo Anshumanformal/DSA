e931a1e48ba78cd34c4032bd74f7ee351263e240
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-07-24 14:38:44 +0530

    Time: 111 ms (66.95%), Space: 11.1 MB (77.57%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        
+        int n = coins.size();
+        int sum = amount;
+        long long int t[n+1][sum+1];
+
+        //initialization
+        for (int i=0; i<n+1; i++)
+            t[i][0] = 0;
+        
+        for (int j=1; j<=amount; j++)
+            t[0][j] = INT_MAX;
+
+
+        // filling the matrix
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<sum+1; j++){
+                if(coins[i-1] <= j)
+                   t[i][j] = min(1 + t[i][j - coins[i - 1]], t[i - 1][j]);
+                else
+                    t[i][j] = t[i-1][j];
+            }
+        }
+        if(t[n][sum] == INT_MAX)
+            return -1;
+        return t[n][sum];
+        
+    }
+};
