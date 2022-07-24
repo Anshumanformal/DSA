74e016371846df51c90666f3f439811acf5b335f
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-07-24 14:49:31 +0530

    Time: 135 ms (51.05%), Space: 11 MB (77.57%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -10,7 +10,7 @@ public:
         for (int i=0; i<n+1; i++)
             t[i][0] = 0;
         
-        for (int j=1; j<=sum; j++)
+        for (int j=1; j<sum+1; j++)
             t[0][j] = INT_MAX;
 
 
@@ -18,7 +18,7 @@ public:
         for(int i=1; i<n+1; i++){
             for(int j=1; j<sum+1; j++){
                 if(coins[i-1] <= j)
-                   t[i][j] = min(1 + t[i][j - coins[i - 1]], t[i - 1][j]);
+                   t[i][j] = min(1 + t[i][j-coins[i-1]] , t[i-1][j]);
                 else
                     t[i][j] = t[i-1][j];
             }
