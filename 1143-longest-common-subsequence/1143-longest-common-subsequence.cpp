61953e4c73559bbe9d1fbf28eeb1a67167324284
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-07-25 21:20:08 +0530

    Time: 14 ms (89.36%), Space: 10.5 MB (88.04%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    
+       // int t[1001][1001];
+//     Tabular solution
     int lcs(string X, string Y, int m, int n){
         // Create matrix
         int t[m+1][n+1];
@@ -23,10 +24,34 @@ public:
         return t[m][n];
     }
     
+//     Recursive + Memoization solution
+//     int lcs(string X, string Y, int m, int n){
+
+//         //base case
+//         if(m == 0 || n == 0)
+//             return 0;
+
+//         if(t[m][n] != -1)
+//             return t[m][n];
+
+//         // when last character is same for both subsequences
+//         if(X[m-1] == Y[n-1])
+//             // adding 1 because this option is selected. 1 is for selection. When we haven't selected, we are taking max of the values.
+//             return t[m][n] = 1 + lcs(X,Y,m-1,n-1);
+
+//         // when last characters aren't same
+//         else if(X[m-1] != Y[n-1])
+//             // taking max as we have to get the maximum/longest common subsequence
+//             return t[m][n] = max(lcs(X,Y,m,n-1), lcs(X,Y,m-1,n));
+
+//         return 0;
+//     }
+    
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.size();
         int n = text2.size();
-        
+        // memset(t,-1,sizeof(t));
+
         return lcs(text1, text2, m, n);
     }
 };
