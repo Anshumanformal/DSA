6b51c3d5628b818b0d0beace42d45c98edc4050d
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-07-28 18:54:09 +0530

    Time: 52 ms (26.57%), Space: 12.4 MB (17.72%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -9,6 +9,7 @@ public:
         //creating the dp matrix;
         vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
         
+        // filling the dp matrix
         for(int i=1; i<m+1; i++){
             for(int j=1; j<n+1; j++){
                 if(word1[i-1] == word2[j-1])
@@ -19,6 +20,8 @@ public:
             }
         }
         
-        return m - dp[m][n] + n - dp[m][n];
+        // int number_of_deletions = m - lcs_length; // m - dp[m][n]
+        // int number_of_insertions = n - lcs_length;// n - dp[m][n]
+        return m - dp[m][n] + n - dp[m][n]; // number of insertions + number of deletions
     }
 };
