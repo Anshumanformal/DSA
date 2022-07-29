8afe4ee16f6e27f1caf7f712a54ebbe10cc3d059
Anshuman Ranjan <anshumanranjan1998@gmail.com>
2022-07-29 17:43:20 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (73.12%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         
-        int size = matrix.size();
-        
-        for(int i=0; i<size; i++){
-            for(int j=0; j<i; j++){
-                swap(matrix[i][j], matrix[j][i]); // transpose of the matrix
-            }
+    int N = matrix[0].size();
+    for (int i = 0; i < N / 2; i++) {
+        for (int j = i; j < N - i - 1; j++) {
+ 
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[N - 1 - j][i];
+            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j];
+            matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i];
+            matrix[j][N - 1 - i] = temp;
         }
-        
-        for(int i=0; i<size; i++)
-            reverse(matrix[i].begin(), matrix[i].end());    // reversing every row to get the result
-        
-        // swap function - It applies on the given values only. It only swaps the supplied values.
-        // reverse function - It applies on a range and reverses the complete range
+    }
     }
 };
